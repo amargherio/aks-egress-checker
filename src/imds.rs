df203f8b7bb9431d5f2ad3434a52ba2eea64858c
Adam Margherio <margherio.adam@gmail.com>
2023-06-10 15:48:16 -0500

    updated imds response handling logic and added first pass at tests

--- src/imds.rs
+++ src/imds.rs
@@ -1,33 +1,108 @@
 use anyhow::{anyhow, Result};
+use log::{error, info, debug, warn};
 use serde_json::Value;
-use tracing::event;
+
+const MAX_RETRY_COUNT: u8 = 5;
 
 /// Queries Azure IMDS for the region of the deployed VMs.
 /// Short and sweet, bailing quick if something weird happens.
 #[tracing::instrument()]
-pub async fn get_region() -> Result<String> {
-    event!(tracing::Level::INFO, "Querying IMDS for the Azure region the node is deployed in. This is used to build the URLs for the connectivity checks.");
+pub async fn get_region(imds_host: &str) -> Result<String> {
+    info!("Querying IMDS for the Azure region the node is deployed in. This is used to build the URLs for the connectivity checks.");
     let client = reqwest::Client::new();
 
-    let res = client
-        .get("http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-        .header("Metadata", "true")
-        .send()
-        .await?;
+    let mut is_retriable = true;
+    let mut retry_count: u8 = 0;
+    let mut region: String = String::from("");
 
-    if res.status().is_client_error() || res.status().is_server_error() {
-        // handle an issue with the client making the request
-        todo!(
-            "Handle an issue with the client making the request or a server error in the response"
-        );
-    }
+    let imds_url = format!("http://{}/metadata/instance?api-version=2021-12-13", imds_host);
+
+    while is_retriable {
+        debug!("Attempting to get IMDS response for VM from {}", imds_url);
+        let res = client
+            .get(&imds_url)
+            .header("Metadata", "true")
+            .send()
+            .await?;
+
+        if res.status().is_client_error() || res.status().is_server_error() {
+            match res.status().as_u16() {
+                400 => {
+                    error!("IMDS query is missing the Metadata header or ")
+                },
+                404|405 => {
+                    // Resource not found or method not allowed. No point in attempting a retry
+                    // so return an error and exit.
+                    error!("VM instance metadata was not found or the GET method was not accepted by IMDS.");
+                    return Err(anyhow!(
+                        "IMDS query failed with status code {}, retry is not applicable. Response body: {:?}",
+                        res.status().as_u16(),
+                        res.text().await?
+                    ));
+                },
+                410|429|500 => {
+                    // These three error codes represent:
+                    //  - 410 - Retry after some time with a max backoff of 70 seconds
+                    //  - 429 API rate limits have been exceeded (5 requests per second as documented at https://learn.microsoft.com/en-us/azure/virtual-machines/instance-metadata-service?tabs=linux#rate-limiting)
+                    //  - 500 - Internal server error, retry after some time but no max backoff specified
+                    //
+                    // Log the response status we got with appropriate messaging and then try again.
+                    warn!(
+                        "IMDS returned a {} response, retrying after some time.",
+                        res.status().as_u16()
+                    );
+
+                    if retry_count > MAX_RETRY_COUNT {
+                        error!("Retry count exceeded. Returning error and exiting.");
+                        return Err(anyhow!(
+                            "Retry count exceeded."
+                        ));
+                    }
+
+                    // Increment the retry count and calculate the sleep duration (10 * retry count, up to max retry count of 5)
+                    retry_count += 1;
+                    let sleep_dur = 10 * retry_count;
+
+                    tokio::time::sleep(std::time::Duration::from_secs(sleep_dur.into())).await;
+                    continue;
+                }
+                _ => {
+                    // Unexpected HTTP status code from IMDS, log and error out.
+                    error!("Unexpected HTTP status code from IMDS, logging error details and exiting.");
+                    return Err(anyhow!(
+                        "Unexpected HTTP status code from IMDS, logging error details and exiting. Status: {}, response body: {:?}",
+                        res.status().as_u16(),
+                        res.text().await?
+                    ));
+                }
+            }
+            // handle an issue with the client making the request
+            if res.status().is_client_error() {
+                error!("Bad client request made to IMDS, logging error details and exiting.");
+                return Err(anyhow!(
+                    "Request to IMDS for instance metadata returned a client error response. Status: {}, response body: {:?}",
+                    res.status().as_u16(),
+                    res.text().await?
+                ));
+            }
+
+            if res.status().is_server_error() {
+                error!("Bad server response from IMDS, logging error details and exiting.");
+                return Err(anyhow!(
+                    "Request to IMDS for instance metadata returned a server error response. Status: {}, response body: {:?}",
+                    res.status().as_u16(),
+                    res.text().await?
+                ));
+            }
+        }
 
         let res_payload = res.text().await?;
+        is_retriable = false;
 
         debug!("IMDS response: {:?}", res_payload);
         let val: Value = serde_json::from_str(res_payload.as_str())?;
         let rval = val["compute"]["location"].as_str().unwrap();
-        let region = String::from(rval);
+        region = String::from(rval);
 
         if region.is_empty() {
             error!("Azure region was not returned in the IMDS response - response received: {:?}", res_payload);
@@ -37,6 +112,8 @@ pub async fn get_region() -> Result<String> {
         ));
         }
     }
+
+    info!("IMDS query successful, region: {}", region);
 
     Ok(region)
 }
@@ -67,18 +144,9 @@ mod test {
             .mount(&mock_server)
             .await;
 
-        let client = reqwest::Client::new();
-        let res = client
-            .get(format!("{}/metadata/instance?api-version=2021-02-01", &mock_server.uri()))
-            .header("Metadata", "true")
-            .send()
-            .await.unwrap();
+        let region = get_region(&mock_server.address().to_string().as_str()).await.unwrap();
 
-        let res_payload = res.text().await.unwrap();
-        let val: Value = serde_json::from_str(res_payload.as_str()).unwrap();
-        let region = val["compute"]["location"].as_str().unwrap();
-
-        assert_eq!("eastus2", region);
+        assert_eq!("eastus2", region.as_str());
     }
 
     #[tokio::test]
@@ -89,18 +157,13 @@ mod test {
         let mock = Mock::given(method("GET"))
             .and(path("/metadata/instance"))
             .and(header("Metadata", "true"))
-            .respond_with(ResponseTemplate::new(503)
-                .set_body_raw(imds_resp.as_str(), "application/json; charset=utf-8")
-            )
+            .respond_with(ResponseTemplate::new(500))
             .mount(&mock_server)
             .await;
 
-        let client = reqwest::Client::new();
-        let res = client
-            .get(format!("{}/metadata/instance?api-version=2021-02-01", &mock_server.uri()))
-            .header("Metadata", "true")
-            .send()
-            .await.unwrap();
+        let region = get_region(&mock_server.address().to_string().as_str()).await;
+
+
     }
 
     #[tokio::test]
